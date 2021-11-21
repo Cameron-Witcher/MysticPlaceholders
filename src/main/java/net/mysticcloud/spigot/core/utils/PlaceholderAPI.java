a777404eb54701adab394e843c2e564ea52ed633
Cameron-Witcher <CameronWitcher1@gmail.com>
2021-11-21 17:55:33 -0600

    tests

--- src/main/java/net/mysticcloud/spigot/core/utils/PlaceholderAPI.java
+++ src/main/java/net/mysticcloud/spigot/core/utils/PlaceholderAPI.java
@@ -34,6 +34,7 @@ public class PlaceholderAPI {
 			// $fade:FFFF00:FF6666:This is the message-fade$
 			String info = "";
 //				try {
+			Utils.log("Looking for " + "$" + e.getKey() + ":");
 			info = tmp.split("$" + e.getKey() + ":")[1].split("-" + e.getKey() + "$")[0];
 			Utils.log("First one worked. Info: " + info);
 //				} catch (ArrayIndexOutOfBoundsException ex) {
